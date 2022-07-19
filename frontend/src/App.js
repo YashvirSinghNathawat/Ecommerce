7a90e4532742bbcbfd1959f2330cfd01b6ea4931
Yashvir <yashveernathawat@gmail.com>
2022-07-20 00:35:04 +0530

    Profile Page Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
 import Search from "./component/Product/Search.js";
 import LoginSignUp from "./component/User/LoginSignUp";
+import Profile from "./component/User/Profile.js";
 import store from "./store";
 import UserOptions from "./component/layout/Header/UserOptions.js";
 import { loadUser } from "./actions/userAction";
@@ -35,6 +36,7 @@ function App() {
         <Route exact path="/products" element={<Products />} />
         <Route path="/products/:keyword" element={<Products />} />
         <Route exact path="/search" element={<Search />} />
+        <Route exact path="/account" element={<Profile />} />
         <Route exact path="/login" element={<LoginSignUp />} />
       </Routes>
       <Footer />
