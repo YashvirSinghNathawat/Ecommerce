a2f46d8d1e8f100ba9fe8ddcc8d529fd90939185
Yashvir <yashveernathawat@gmail.com>
2022-07-19 23:04:19 +0530

    SignIn and LogOut

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,26 @@ import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
 import Search from "./component/Product/Search.js";
 import LoginSignUp from "./component/User/LoginSignUp";
+import store from "./store";
+import UserOptions from "./component/layout/Header/UserOptions.js";
+import { loadUser } from "./actions/userAction";
+import { useSelector } from "react-redux";
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
   React.useEffect(() => {
     WebFont.load({
       google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
+    store.dispatch(loadUser());
   }, []);
   return (
     // Components
     <Router>
       <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/product/:id" element={<ProductDetails />} />
